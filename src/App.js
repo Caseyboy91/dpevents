748bfce995161ba6f6ceb4de59bba3eccd085eb0
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-03-01 09:30:43 -0800

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes></Routes>
       </BrowserRouter>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="footer sticky-bottom">
+      <div className="container">
+        <div className="row align-itmes-center">
+          <div className="col">
+            <p>Diane Pugmire Events</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
