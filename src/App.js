3a3acb5a333d670b227b297317db1e4076f27eae
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-03-01 12:37:44 -0800

    create about page. add initial content on about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </BrowserRouter>
       </body>
---
+++ src/pages/About/About.js
@@ -0,0 +1,30 @@
+const About = () => {
+  return (
+    <section className="container min-vh-100 py-3">
+      <div className="row w-75 mx-auto d-flex flex-column align-items-center">
+        <div className="col d-flex flex-column align-items-center">
+          <h1 className="text-uppercase">About</h1>
+          <p className="text-uppercase">diane pugmire</p>
+        </div>
+        <div className="col d-flex flex-column align-items-center">
+          <p className="text-uppercase">
+            we create memorable guest experiences
+          </p>
+          <p className="text-center">
+            At Diane Pugmire Events your guest experience encompasses many
+            different adjectives like fun, energetic, classic, romantic,
+            intimate, thoughtful, and colorful. Any of our vendors would tell
+            you as wedding & event planners, we are incredibly detail-oriented,
+            work with in a budget, hard-working, honest and reliable. Our
+            clients would tell you that their wedding or event reflected 100%
+            who they are and their style. Because from the beginning to end; we
+            want to know who you are. What you desire and what speaks to your
+            personality and brand.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
