4d2d490324e4fd5c2ba1f93f6c07f6782d89250c
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-03-02 13:54:55 -0800

    update styling for mobile view

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,30 @@ const Footer = () => {
       <div className="container">
         <div className="row align-itmes-center w-75 mx-auto">
           <div className="col d-flex justify-content-between">
-            <span className="pe-3">Diane Pugmire Events</span>
+            <span className="pe-3">DP Events</span>
             <a
               href="mailto:diane@hellodpevents.com"
               className="text-decoration-none text-reset pe-3"
             >
-              <img src={Email} alt="email" className="pe-2" />
-              diane@hellodpevents.com
+              <img
+                src={Email}
+                alt="email"
+                className="pe-2 d-md-none d-lg-inline"
+              />
+              <span className="d-none d-md-inline">
+                diane@hellodpevents.com
+              </span>
             </a>
             <a
               href="tel:+12086313650"
               className="text-decoration-none text-reset"
             >
-              <img src={Phone} alt="phone number" className="pe-2" />
-              208.631.3650
+              <img
+                src={Phone}
+                alt="phone number"
+                className="pe-2 d-md-none d-lg-inline"
+              />
+              <span className="d-none d-md-inline">208.631.3650</span>
             </a>
           </div>
         </div>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,14 +4,56 @@ import "./About.css";
 const About = () => {
   return (
     <section className="about container min-vh-100 py-3 ">
-      <div className="row w-75 mx-auto d-flex flex-column align-items-center">
+      <div className="row w-md-75 mx-md-auto d-flex flex-column align-items-center">
         <div className="col d-flex flex-column align-items-center">
           <p className="lead fs-1 text-uppercase fw-lighter">about</p>
         </div>
         <hr></hr>
       </div>
-      <div className="row align-items-center w-75 mx-auto">
-        <div className="col-4">
+      <div className="row align-items-center w-md-75 mx-md-auto">
+        <div className="col-12 d-flex justify-content-center d-md-none">
+          <figure className="figure text-center">
+            <img
+              src={Diane}
+              alt="Diane Pugmire"
+              className="figure-img img-fluid rounded shadow"
+            />
+          </figure>
+        </div>
+        <div className="col-12 text-center d-md-none">
+          <p className="lead fs-2">Hi, I'm Diane!</p>
+          <p className="lead fs-6 fst-italic">
+            Born and raised in the vibrant city of Las Vegas, Nevada, my heart
+            has always been in Boise, Idaho where I have spent most of my life.
+            I am beyond blessed with a wonderful family that includes amazing
+            children and grandchildren who are an integral part of my life.
+          </p>
+          {/* <p className="lead fs-6 fst-italic">
+            Volunteering is a passion of mine, and I am proud to give back to
+            the Boise community by supporting local charities, including the
+            Idaho Veteran's Garden in Caldwell, ID.
+          </p> */}
+          {/* <p className="lead fs-6 fst-italic">
+            When I'm not planning unforgettable events, you can find me enjoying
+            quality time with my girlfriends, relishing in the joy of my
+            grandchildren, cooking up new and exciting dishes for my family, or
+            savoring a delicious cup of coffee with my husband.
+          </p> */}
+          <p className="lead fs-6 fst-italic">
+            One of the highlights of my career in the event industry was the
+            opportunity to work alongside the legendary Preston Bailey and David
+            Tutera as a Coordinator for Interns at their "Your Wedding
+            Experience" wedding show in Philadelphia, PA and Fort Lauderdale,
+            FL.
+          </p>
+          <p className="lead fs-6 fst-italic">
+            As an event planner, I am passionate about bringing my client's
+            visions to life and sharing my creativity to make people smile. I
+            can't wait to meet with you and embark on a creative journey
+            together!
+          </p>
+        </div>
+        <div className="col-4 d-none d-md-inline">
           <figure className="figure text-center">
             <img
               src={Diane}
@@ -23,7 +65,7 @@ const About = () => {
             </figcaption>
           </figure>
         </div>
-        <div className="col-7">
+        <div className="col-7 d-none d-md-inline">
           <p className="lead fs-2">Hi, I'm Diane!</p>
           <p className="lead fs-6 fst-italic">
             Born and raised in the vibrant city of Las Vegas, Nevada, my heart
