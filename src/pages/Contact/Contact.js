20d26fb278e82ecb02bc2541d7bd7287e73fa105
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-03-03 13:01:41 -0800

    initial commit

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <section className="container contact align-items-center min-vh-100 py-3">
+      <div className="row">
+        <div className="col">
+          <p className="lead text-uppercase tw-lighter fs-1">contact</p>
+        </div>
+      </div>
+    </section>
+  );
+};
